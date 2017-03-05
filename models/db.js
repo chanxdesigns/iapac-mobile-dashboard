eb34cb1ac75a77fe1f966bd477058773a4292bad
Chanx Singha <chanx.designs@gmail.com>
2017-03-05 10:29:33 +0530

    Upgraded MySQL single connection instance to Connection Pool

--- models/db.js
+++ models/db.js
@@ -1,24 +1,15 @@
 var mysql = require('mysql');
 
-    /**
-     * MySqli Connection Object
-     * @type {Connection}
-     */
+/**
+ * MySqli Connection Pool Object
+ * @type {Pool}
+ */
 
-var conn = mysql.createConnection({
+var conn = mysql.createPool({
     host: process.env.MYSQL_HOST || "43.225.55.90",
     user: process.env.MYSQL_USER || "cresezg1_chanx",
     password: process.env.MYSQL_PASS || "shekhar12",
-    database: process.env.DATABASE || "cresezg1_laravel",
-    timeout: 40000
+    database: process.env.DATABASE || "cresezg1_laravel"
 });
-
-/**
- * Initiate Connection
- */
-
-conn.connect(function (err) {
-    if (err) res.status(500).end(err.message);
-})
 
 module.exports = conn;
--- routes/api.js
+++ routes/api.js
@@ -7,23 +7,32 @@ var moment = require('moment');
  * Get All Project IDs
  */
 router.get('/projectids', function (req, res) {
-    db.query('SELECT DISTINCT projectid FROM resp_counters ORDER BY created_at DESC', function (err, results) {
-        if (err) {
-            res.status(500).end(err.message)
-        } else {
-            res.json(results);
-        }
-    });
+    db.getConnection(function (err, conn) {
+        if (err) res.status(500).end(err.message);
+        conn.query('SELECT DISTINCT projectid FROM resp_counters ORDER BY created_at DESC', function (err, results) {
+            // Release connection
+            conn.release();
+            if (err) {
+                res.status(err.code).end(err.message);
+            } else {
+                res.json(results);
+            }
+        });
+    })
 });
 
 router.get('/countries', function (req, res) {
-    db.query('SELECT DISTINCT Country FROM projects_list', function (err, results) {
-        if (err) {
-            res.status(500).end(err.message);
-        } else {
-            res.json(results);
-        }
-    })
+    db.getConnection(function (err, conn) {
+        conn.query('SELECT DISTINCT Country FROM projects_list', function (err, results) {
+            // Release connection
+            conn.release();
+            if (err) {
+                res.status(500).end(err.message);
+            } else {
+                res.json(results);
+            }
+        })
+    });
 });
 
 
@@ -32,26 +41,30 @@ router.get('/countries', function (req, res) {
  */
 
 router.get('/:projectid', function (req, res) {
-    db.query('SELECT * FROM resp_counters WHERE projectid = ? ORDER BY created_at DESC', req.params.projectid, function (err, results) {
-        if (err) {
-            res.status(500).end(err.message)
-        } else {
-            var results = results.map(function (val) {
-                var starttime = moment(val.starttime),
-                    endtime = moment(val.enddate);
-                return {
-                    projectid: val.projectid,
-                    respid: val.respid,
-                    status: val.status,
-                    country: val.Languageid,
-                    ip: val.IP,
-                    starttime: starttime.format('ddd, MMM D YYYY, hh:mm A'),
-                    endtime: endtime.format('ddd, MMM D YYYY, hh:mm A')
-                }
-            });
+    db.getConnection(function (err, conn) {
+        conn.query('SELECT * FROM resp_counters WHERE projectid = ? ORDER BY created_at DESC', req.params.projectid, function (err, results) {
+            // Release connection
+            conn.release();
+            if (err) {
+                res.status(500).end(err.message)
+            } else {
+                var results = results.map(function (val) {
+                    var starttime = moment(val.starttime),
+                        endtime = moment(val.enddate);
+                    return {
+                        projectid: val.projectid,
+                        respid: val.respid,
+                        status: val.status,
+                        country: val.Languageid,
+                        ip: val.IP,
+                        starttime: starttime.format('ddd, MMM D YYYY, hh:mm A'),
+                        endtime: endtime.format('ddd, MMM D YYYY, hh:mm A')
+                    }
+                });
 
-            res.json(results);
-        }
+                res.json(results);
+            }
+        });
     });
 });
 
@@ -63,27 +76,31 @@ router.get('/:projectid/:status/:country', function (req, res) {
         status = req.params.status,
         country = req.params.country;
 
-    db.query('SELECT * FROM resp_counters WHERE projectid = ? AND status = ? AND Languageid = ? ORDER BY created_at DESC', [projectid, status, country], function (err, results) {
-        if (err) {
-            res.status(500).end(err.message);
-        } else {
-            var results = results.map(function (val) {
-                var starttime = moment(val.starttime),
-                    endtime = moment(val.enddate);
-                return {
-                    projectid: val.projectid,
-                    respid: val.respid,
-                    status: val.status,
-                    country: val.Languageid,
-                    ip: val.IP,
-                    starttime: starttime.format('ddd, MMM D YYYY, hh:mm A'),
-                    endtime: endtime.format('ddd, MMM D YYYY, hh:mm A')
-                }
-            });
+    db.getConnection(function (err, conn) {
+        conn.query('SELECT * FROM resp_counters WHERE projectid = ? AND status = ? AND Languageid = ? ORDER BY created_at DESC', [projectid, status, country], function (err, results) {
+            // Release connection
+            conn.release();
+            if (err) {
+                res.status(500).end(err.message);
+            } else {
+                var results = results.map(function (val) {
+                    var starttime = moment(val.starttime),
+                        endtime = moment(val.enddate);
+                    return {
+                        projectid: val.projectid,
+                        respid: val.respid,
+                        status: val.status,
+                        country: val.Languageid,
+                        ip: val.IP,
+                        starttime: starttime.format('ddd, MMM D YYYY, hh:mm A'),
+                        endtime: endtime.format('ddd, MMM D YYYY, hh:mm A')
+                    }
+                });
 
-            res.json(results);
-        }
-    })
+                res.json(results);
+            }
+        });
+    });
 })
 
 module.exports = router;
